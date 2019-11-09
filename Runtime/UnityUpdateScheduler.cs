c8e41113595ac2927c7889a0fc66085fc9271316
JackSParrot <franciscoruizpons@gmail.com>
2019-11-09 18:16:53 +0100

    Update UnityUpdateScheduler.cs

--- Runtime/UnityUpdateScheduler.cs
+++ Runtime/UnityUpdateScheduler.cs
@@ -1,40 +1,35 @@
-﻿using System;
+using System;
 using UnityEngine;
 
 namespace JackSParrot.Utils
 {
-    public class UnityUpdateScheduler : IUpdateScheduler
+    public class UnityUpdateScheduler : IUpdateScheduler, IDisposable
     {
         class Updater : MonoBehaviour
         {
-            static Updater _instance;
-            public static Updater Instance
-            {
-                get
-                {
-                    if (_instance == null)
-                    {
-                        _instance = new GameObject("UpdateRunner").AddComponent<Updater>();
-                        DontDestroyOnLoad(_instance.gameObject);
-                    }
-                    return _instance;
-                }
-            }
             public event Action<float> OnUpdate;
             void Update()
             {
                 OnUpdate?.Invoke(Time.deltaTime);
             }
         }
+        Updater _updater = null;
 
         public UnityUpdateScheduler()
         {
-            Updater.Instance.OnUpdate += Update;
+            _updater = new GameObject("UpdateRunner").AddComponent<Updater>();
+            UnityEngine.Object.DontDestroyOnLoad(_updater.gameObject);
+            _updater.OnUpdate += Update;
         }
 
         protected override void Update(float dt)
         {
             base.Update(dt);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(_updater.gameObject);
         }
     }
 }
