e54c555a285d50a7f6f67a574b08ea65280e6e27
JackSParrot <franciscoruizpons@gmail.com>
2021-03-17 08:11:18 +0100

    Update EventDispatcher.cs

--- Runtime/EventDispatcher.cs
+++ Runtime/EventDispatcher.cs
@@ -1,75 +1,137 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace JackSParrot.Utils
 {
     public class EventDispatcher : IDisposable
     {
-        public delegate void Listener<T>(T e) where T : class;
+        private class Listener
+        {
+            public Type EventType;
+            public Delegate EventDelegate;
+        }
 
         readonly Dictionary<Type, List<Delegate>> _listeners = new Dictionary<Type, List<Delegate>>();
-        
-        public void Clear()
-        {
-            _listeners.Clear();
-        }
+        readonly List<Listener> _listenersToAdd = new List<Listener>();
+        readonly List<Listener> _listenersToRemove = new List<Listener>();
+        readonly List<dynamic> _eventsToRaise = new List<dynamic>();
+        private bool _processing = false;
 
-        public void AddListener<T>(Listener<T> listener) where T : class
+        public void AddListener<T>(Action<T> listener) where T : class
         {
-            List<Delegate> d;
-            if(!_listeners.TryGetValue(typeof(T), out d))
+            var evListener = new Listener { EventType = typeof(T), EventDelegate = listener };
+            if(_processing)
             {
-                d = new List<Delegate>();
-                _listeners[typeof(T)] = d;
+                _listenersToAdd.Add(evListener);
             }
-            if(!d.Contains(listener))
-                d.Add(listener);
+            else
+            {
+                AddListenerInternal(evListener);
+            }
         }
 
-        public void RemoveListener<T>(Listener<T> listener) where T : class
+        public void RemoveListener<T>(Action<T> listener) where T : class
         {
-            List<Delegate> group;
-            if(_listeners.TryGetValue(typeof(T), out group))
+            var evListener = new Listener { EventType = typeof(T), EventDelegate = listener };
+            if (_processing)
             {
-                group.Remove(listener);
+                _listenersToRemove.Add(evListener);
+            }
+            else
+            {
+                RemoveListenerInternal(evListener);
             }
         }
 
         public void Raise<T>() where T : class, new()
         {
-            Raise<T>(new T());
+            Raise(new T());
         }
 
         public void Raise<T>(T e) where T : class
         {
-            if(e == null)
+            if(_processing)
             {
-                SharedServices.GetService<ICustomLogger>()?.LogError("Raised event with a null parameter");
-                return;
+                _eventsToRaise.Add(e);
             }
-
-            List<Delegate> listeners;
-            if(!_listeners.TryGetValue(typeof(T), out listeners))
+            else
             {
-                SharedServices.GetService<ICustomLogger>()?.LogDebug("Raised event with no listeners");
-                return;
-            }
-            listeners = new List<Delegate>(listeners);
-
-            for(int i = 0; i < listeners.Count; ++i)
-            {
-                var callback = listeners[i] as Listener<T>;
-
-                if(callback != null)
+                InternalRaise(e);
+                while(_eventsToRaise.Count > 0)
                 {
-                    callback(e);
+                    InternalRaise(_eventsToRaise[0]);
+                    _eventsToRaise.RemoveAt(0);
                 }
             }
+        }
+
+        void InternalRaise<T>(T e) where T : class
+        {
+            UnityEngine.Debug.Assert(e != null, "Raised a null event");
+
+            Type type = e.GetType();
+            if (!_listeners.TryGetValue(type, out List<Delegate> listeners))
+            {
+                UnityEngine.Debug.Log("Raised event with no listeners");
+                return;
+            }
+
+            _processing = true;
+            listeners.RemoveAll(e => e == null);
+            foreach(Delegate listener in listeners)
+            {
+                if (listener is Action<T> castedDelegate)
+                {
+                    castedDelegate(e);
+                }
+            }
+            _processing = false;
+
+            foreach (var listenerToAdd in _listenersToAdd)
+            {
+                AddListenerInternal(listenerToAdd);
+            }
+            _listenersToAdd.Clear();
+
+            foreach (var listenerToRemove in _listenersToRemove)
+            {
+                RemoveListenerInternal(listenerToRemove);
+            }
+            _listenersToRemove.Clear();
         }
 
         public void Dispose()
         {
             Clear();
         }
+
+        public void Clear()
+        {
+            _listeners.Clear();
+            _listenersToAdd.Clear();
+            _listenersToRemove.Clear();
+        }
+
+        #region Internals
+        private void AddListenerInternal(Listener listener)
+        {
+            UnityEngine.Debug.Assert(listener != null, "Added a null listener.");
+            if (!_listeners.TryGetValue(listener.EventType, out List<Delegate> delegateList))
+            {
+                delegateList = new List<Delegate>();
+                _listeners[listener.EventType] = delegateList;
+            }
+            UnityEngine.Debug.Assert(delegateList.Find(e => e == listener.EventDelegate) == null, "Added duplicated event listener to the event dispatcher.");
+            delegateList.Add(listener.EventDelegate);
+        }
+
+        private void RemoveListenerInternal(Listener listener)
+        {
+            if (listener != null && _listeners.TryGetValue(listener.EventType, out List<Delegate> group))
+            {
+                group.RemoveAll(e => e == listener.EventDelegate);
+            }
+        }
+        #endregion
     }
 }
